923bdf90470f010b4500f08e744920e15d4b0e56
AshrafulIsalm34 <112802887+AshrafulIsalm34@users.noreply.github.com>
2022-09-16 17:11:17 +0600

    Add files via upload

---
+++ js/coustom.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+	// counter up start
+	 $(".count").counterUp({
+	 	delay:10,
+	 	time:3000,
+	 });
+
+//stiky top menu js
+    $(window).scroll(function () {
+        var scrolling = $(this).scrollTop();
+        var stiky = $('.stiky-top-menu');
+
+        if (scrolling >= 200) {
+            stiky.addClass('nav-bg');
+        } else {
+            stiky.removeClass('nav-bg');
+        }
+    })
+
+// Smooths scrolling
+  $('body').scrollspy({target: ".navbar", offset: 50});
+    
+
+//animation scroll js
+var html_body = $('html, body');
+$('nav a').on('click', function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            html_body.animate({
+                scrollTop: target.offset().top - 50
+            }, 1500);
+            return false;
+        }
+    }
+});
+
+// wow js
+new WOW().init();
+
+// typed js start************************************
+$(".typed").typed({
+		strings: ["Web Designer.", "Web Developer.", "Full Stack Developer.","Graphic Designer"],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 50,
+		// time before typing starts
+		startDelay: 1500,
+		// backspacing speed
+		backSpeed: 10,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: 100,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+// typed js end************************************
+
+// partical js start*******************************
+particlesJS("particles-js", {"particles":{"number":{"value":400,"density":{"enable":true,"value_area":800}},"color":{"value":"#fff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":8.33451405615796,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":500,"color":"#ffffff","opacity":0.4,"width":2},"move":{"enable":true,"speed":3.333805622463184,"direction":"bottom","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"bubble"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":0.5}},"bubble":{"distance":400,"size":4,"duration":0.3,"opacity":1,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+// partical js end*******************************
+
+
+
+
+});
